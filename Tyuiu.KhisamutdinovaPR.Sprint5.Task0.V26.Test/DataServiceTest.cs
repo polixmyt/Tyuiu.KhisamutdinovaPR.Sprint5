c0cd5a90fcb5c2ccd5777caac58979f050e304fa
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-11-14 14:38:07 +0500

    Task0

---
+++ Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,41 @@
+﻿// Author: Хисамутдинова Полина
+// Project: Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26
+// Description: Тест метода SaveToFileTextData.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Globalization;
+using System.IO;
+using Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26.Lib;
+
+namespace Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void SaveToFileTextData_CreatesFile_WithCorrectValue()
+        {
+            // arrange
+            int x = 2;
+            DataService ds = new DataService();
+
+            double expected = Math.Round(
+                0.7 * Math.Pow(x, 3) + 1.52 * Math.Pow(x, 2),
+                3);
+
+            // act
+            string path = ds.SaveToFileTextData(x);
+
+            // assert: файл существует
+            Assert.IsTrue(File.Exists(path), "Файл не был создан.");
+
+            string text = File.ReadAllText(path);
+
+            // парсим с текущей культурой (запятая/точка)
+            double actual = double.Parse(text, CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(expected, actual, 0.0001, "Значение функции записано неверно.");
+        }
+    }
+}
---
+++ Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26/Program.cs
@@ -0,0 +1,47 @@
+﻿// Author: Хисамутдинова Полина
+// Project: Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26
+// Description: Вызов библиотеки, вывод пути к файлу и его содержимого.
+
+using System;
+using System.IO;
+using Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26.Lib;
+
+namespace Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт 5 | Таск 0 | Вариант 26";
+
+            DataService ds = new DataService();
+
+            int x = 2;
+
+            string path = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Спринт 5 | Таск 0 | Вариант 26");
+            Console.WriteLine("Выражение: F(x) = 0.7x^3 + 1.52x^2");
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($"Результат записан в файл:");
+            Console.WriteLine(path);
+            Console.WriteLine("----------------------------------------");
+
+            if (File.Exists(path))
+            {
+                string text = File.ReadAllText(path);
+                Console.WriteLine("Содержимое файла:");
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден!");
+            }
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
