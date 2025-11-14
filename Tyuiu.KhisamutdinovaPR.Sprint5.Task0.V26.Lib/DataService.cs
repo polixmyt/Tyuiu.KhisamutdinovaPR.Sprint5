2176a0441309cb17954eea8ba972e23405979137
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-11-14 14:46:25 +0500

    Task0

--- Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26.Lib/DataService.cs
@@ -1,8 +1,4 @@
-﻿// Author: Хисамутдинова Полина
-// Project: Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26
-// Description: Вычисление F(x) и сохранение результата в текстовый файл.
-
-using System;
+﻿using System;
 using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
@@ -10,28 +6,24 @@ namespace Tyuiu.KhisamutdinovaPR.Sprint5.Task0.V26.Lib
 {
     public class DataService : ISprint5Task0V26
     {
-        /// <summary>
-        /// Вычисляет значение функции F(x) = 0.7x^3 + 1.52x^2,
-        /// округляет до трех знаков, сохраняет в файл OutPutFileTask0.txt
-        /// во временной папке и возвращает полный путь к файлу.
-        /// </summary>
-        /// <param name="x">Аргумент функции.</param>
-        /// <returns>Полный путь к созданному файлу.</returns>
         public string SaveToFileTextData(int x)
         {
-            // 1. Вычисляем значение функции
+            // 1. Вычисляем значение функции F(x) = 0.7x^3 + 1.52x^2
             double value = 0.7 * Math.Pow(x, 3) + 1.52 * Math.Pow(x, 2);
-            value = Math.Round(value, 3);
 
-            // 2. Формируем путь: temp-папка + имя файла
-            string tempDir = Path.GetTempPath();          // системная временная директория
-            string fileName = "OutPutFileTask0.txt";      // имя по условию
+            // 2. Округляем ДО ДВУХ знаков
+            value = Math.Round(value, 2);
+
+            // 3. Путь к файлу во временной папке
+            string tempDir = Path.GetTempPath();
+            string fileName = "OutPutFileTask0.txt";
             string fullPath = Path.Combine(tempDir, fileName);
 
-            // 3. Записываем значение в файл (как текст, с 3 знаками)
-            File.WriteAllText(fullPath, value.ToString("F3"));
+            // 4. Записываем в файл строку с ДВУМЯ знаками после запятой
+            // "F2" -> 2 знака (в русской культуре будет 11,68)
+            File.WriteAllText(fullPath, value.ToString("F2"));
 
-            // 4. Возвращаем путь
+            // 5. Возвращаем путь к файлу
             return fullPath;
         }
     }
