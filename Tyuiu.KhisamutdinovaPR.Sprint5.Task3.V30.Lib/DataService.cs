ffac05fa3b464ec63adeef766d0ed2901735ce4f
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-11-23 18:29:31 +0500

    Task3

--- Tyuiu.KhisamutdinovaPR.Sprint5.Task3.V30.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint5.Task3.V30.Lib/DataService.cs
@@ -1,37 +1,30 @@
-﻿// Author: Хисамутдинова Полина
-// Project: Tyuiu.KhisamutdinovaPR.Sprint5.Task3.V30
-// Description: Вычисление выражения и сохранение результата в бинарный файл.
-
-using System;
-using System.IO;
+﻿using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
-
 namespace Tyuiu.KhisamutdinovaPR.Sprint5.Task3.V30.Lib
 {
     public class DataService : ISprint5Task3V30
     {
         public string SaveToFileTextData(int x)
         {
-            // 1. Вычисляем значение y = (x^3 - 1) / (4 * x^2)
-            double y = (Math.Pow(x, 3) - 1) / (4 * Math.Pow(x, 2));
+            string path = Path.GetTempFileName();
 
-            // 2. Округляем до трёх знаков
-            y = Math.Round(y, 3);
+            FileInfo fileinfo = new FileInfo(path);
+            bool filexists = fileinfo.Exists;
 
-            // 3. Формируем путь к бинарному файлу во временной папке
-            string tempDir = Path.GetTempPath();
-            string fileName = "OutPutFileTask3.bin";
-            string fullPath = Path.Combine(tempDir, fileName);
-
-            // 4. Записываем значение как float (Single) в бинарный файл
-            using (FileStream fs = new FileStream(fullPath, FileMode.Create, FileAccess.Write))
-            using (BinaryWriter bw = new BinaryWriter(fs))
+            if (filexists)
             {
-                bw.Write((float)y);
+                File.Delete(path);
             }
 
-            // 5. Возвращаем путь к файлу
-            return fullPath;
+            double x1 = Convert.ToDouble(x);
+
+            double res = (Math.Pow(x, 3) - 1) / (4 * Math.Pow(x, 2));
+            res = Math.Round(res, 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(res));
+            }
+            return path;
         }
     }
 }
